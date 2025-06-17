efe9cfed5a828cc963177c629a4d836deb3da15f
Anthony Bonta <zthun.coding@gmail.com>
2025-06-17 15:24:10 -0500

    feat: tag allows you to decorate any method to take it with a label

---
+++ packages/helpful-reflection/src/tag/tag.spec.ts
@@ -0,0 +1,80 @@
+import { describe, expect, it } from "vitest";
+import { isTagged, ZTag } from "./tag.js";
+
+@ZTag("yellow")
+class NumberFactory {
+  @ZTag("red")
+  public infinity = Infinity;
+
+  @ZTag("black")
+  public get three() {
+    return 3;
+  }
+
+  @ZTag("green")
+  @ZTag("orange")
+  public zero() {
+    return 0;
+  }
+
+  @ZTag("blue")
+  public one() {
+    return 1;
+  }
+}
+
+describe("ZTag", () => {
+  describe("Class", () => {
+    it("should know it has a tag", () => {
+      expect(isTagged("yellow", NumberFactory)).toBeTruthy();
+    });
+
+    it("should know it does not have a tag", () => {
+      expect(isTagged("green", NumberFactory)).toBeFalsy();
+    });
+  });
+
+  describe("Method", () => {
+    it("should know it has a tag", () => {
+      expect(isTagged("green", new NumberFactory(), "zero")).toBeTruthy();
+    });
+
+    it("should know it does not have a tag", () => {
+      expect(isTagged("green", new NumberFactory(), "one")).toBeFalsy();
+    });
+  });
+
+  describe("Property", () => {
+    it("should know it has a tag", () => {
+      expect(isTagged("red", new NumberFactory(), "infinity")).toBeTruthy();
+    });
+
+    it("should know it does not have a tag", () => {
+      expect(isTagged("cyan", new NumberFactory(), "infinity")).toBeFalsy();
+    });
+  });
+
+  describe("Accessor", () => {
+    it("should know it has a tag", () => {
+      expect(isTagged("black", new NumberFactory(), "three")).toBeTruthy();
+    });
+
+    it("should know it does not have a tag", () => {
+      expect(isTagged("white", new NumberFactory(), "three")).toBeFalsy();
+    });
+  });
+
+  describe("Other", () => {
+    it("should not be tagged for null", () => {
+      expect(isTagged("blue", null)).toBeFalsy();
+    });
+
+    it("should not be tagged for intrinsic values", () => {
+      expect(isTagged("green", "decorators-not-supported")).toBeFalsy();
+    });
+
+    it("should not be tagged for top level functions", () => {
+      expect(isTagged("green", () => 0)).toBeFalsy();
+    });
+  });
+});
---
+++ packages/helpful-reflection/src/tag/tag.ts
@@ -0,0 +1,39 @@
+import { firstDefined } from "@zthun/helpful-fn";
+import { defineMetadata, getMetadata } from "reflect-metadata/no-conflict";
+
+const Key = "zthunworks:tags";
+
+export function ZTag(
+  by: string,
+): MethodDecorator & ClassDecorator & PropertyDecorator {
+  return (target: any, property?: string | symbol) => {
+    if (property == null) {
+      // Class.
+      const current = firstDefined({}, getMetadata(Key, target));
+      defineMetadata(Key, { ...current, [by]: true }, target);
+    } else {
+      // Member.
+      const current = firstDefined({}, getMetadata(Key, target, property));
+      defineMetadata(Key, { ...current, [by]: true }, target, property);
+    }
+  };
+}
+
+export function isTagged(
+  by: string,
+  target: any,
+  property?: string | symbol,
+): boolean {
+  if (
+    target == null ||
+    (typeof target !== "function" && typeof target !== "object")
+  ) {
+    return false;
+  }
+
+  const tags = property
+    ? getMetadata(Key, target, property)
+    : getMetadata(Key, target);
+
+  return Object.prototype.hasOwnProperty.call(firstDefined({}, tags), by);
+}
