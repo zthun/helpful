0834d16359b58ab703eeda3dbe3bd49c46e856f3
Anthony Bonta <zthun.coding@gmail.com>
2026-02-08 17:46:21 -0600

    test: simplify

--- packages/helpful-fn/src/culture/culture.spec.ts
+++ packages/helpful-fn/src/culture/culture.spec.ts
@@ -1,13 +1,12 @@
-// @vitest-environment node
 import { describe, expect, it } from "vitest";
-import { culture } from "./culture.mjs";
+import { culture, cultures } from "./culture.mjs";
 
 describe("Culture", () => {
   it("should return the current node locale", () => {
-    // Arrange.
-    // Act.
-    const actual = culture();
-    // Assert.
-    expect(actual).toBeTruthy();
+    expect(culture()).toBeTruthy();
+  });
+
+  it("should return all supported cultures", () => {
+    expect(cultures().length).toBeGreaterThan(0);
   });
 });
