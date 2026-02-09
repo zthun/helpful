ead6e98cbbd85495f054e9405a9786eedb79d676
Anthony Bonta <zthun.coding@gmail.com>
2026-02-08 23:38:52 -0600

    test: we have enums already

--- packages/helpful-fn/src/enum/is-enum.spec.ts
+++ packages/helpful-fn/src/enum/is-enum.spec.ts
@@ -1,71 +1,58 @@
 import { describe, expect, it } from "vitest";
+import { ZVerticalAnchor } from "../anchor/anchor.mjs";
+import { ZCalendarMonth } from "../date/calendar-month.mjs";
 import { isEnum } from "./is-enum.mjs";
-
-enum StringEnum {
-  One = "one",
-  Two = "two",
-}
-
-enum NumberEnum {
-  One = 2,
-  Two = 3,
-}
-
-enum AutoEnum {
-  One,
-  Two,
-}
 
 describe("isEnum", () => {
   it("should be false for null", () => {
-    expect(isEnum(AutoEnum, null)).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, null)).toBeFalsy();
   });
 
   it("should be false for undefined", () => {
-    expect(isEnum(AutoEnum, undefined)).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, undefined)).toBeFalsy();
   });
 
   it("should be false for bigint", () => {
-    expect(isEnum(NumberEnum, 2n)).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, 2n)).toBeFalsy();
   });
 
   it("should be false for functions", () => {
-    expect(isEnum(StringEnum, () => StringEnum.One)).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, () => ZCalendarMonth.February)).toBeFalsy();
   });
 
   it("should be false for symbols", () => {
-    expect(isEnum(AutoEnum, Symbol("One"))).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, Symbol("July"))).toBeFalsy();
   });
 
   it("should be false for booleans", () => {
-    expect(isEnum(AutoEnum, true)).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, true)).toBeFalsy();
   });
 
   it("should be false for objects", () => {
-    expect(isEnum(AutoEnum, { foo: { bar: "baz" } })).toBeFalsy();
+    expect(isEnum(ZCalendarMonth, { foo: { bar: "baz" } })).toBeFalsy();
   });
 
   describe("String", () => {
     it("should be true for values that are in the enum value list", () => {
-      expect(isEnum(StringEnum, StringEnum.Two)).toBeTruthy();
+      expect(isEnum(ZVerticalAnchor, ZVerticalAnchor.Middle)).toBeTruthy();
     });
 
     it("should be false for values that are not in the enum value list", () => {
-      expect(isEnum(StringEnum, "three-hundred")).toBeFalsy();
+      expect(isEnum(ZVerticalAnchor, "half-upper-left")).toBeFalsy();
     });
   });
 
   describe("Number", () => {
     it("should be true for values that are in the enum value list", () => {
-      expect(isEnum(NumberEnum, NumberEnum.Two)).toBeTruthy();
+      expect(isEnum(ZCalendarMonth, ZCalendarMonth.April)).toBeTruthy();
     });
 
     it("should be false for values that are not in the enum value list", () => {
-      expect(isEnum(NumberEnum, 3024)).toBeFalsy();
+      expect(isEnum(ZCalendarMonth, "Jan")).toBeFalsy();
     });
 
     it("should be true for keys as well as values", () => {
-      expect(isEnum(NumberEnum, "One")).toBeTruthy();
+      expect(isEnum(ZCalendarMonth, "February")).toBeTruthy();
     });
   });
 });
